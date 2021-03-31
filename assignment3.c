954ffd1f97d839cac8384655e34c6276cca81a57
이준협 <uncompu14@kyonggi.ac.kr>
2021-03-31 18:37:08 +0900

    초기 커밋.

---
+++ assignment3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int bubble_sort(int arr[], int count);
+
+int main() {
+    int array[6];
+    int bubble;
+    printf("값을 입력하세요 : ");
+    for (int i = 0; i < 6; i++) 
+    {
+        scanf("%d", &array[i]);
+    }
+    bubble = bubble_sort(array, 6);
+    for (int j = 0; j < 6; j++)
+    {
+        printf("%d ", array[j]);
+    }
+}
+
+int bubble_sort(int arr[], int count) {
+    int temp;
+
+    for (int i = 0; i < count; i++)   
+    {
+        for (int j = 0; j < count - 1; j++)  
+        {
+            if (arr[j] > arr[j + 1])          
+            {                                
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;            
+            }
+        }
+    }
+}
